bde977bfc8db3b46c35d41f6a57f8f059b33c265
Techflare <wkabucho4@gmail.com>
2026-05-21 02:14:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
 window.PHV_CONFIG = {
-  BACKEND_BASE_URL: "https://medicback.onrender.com"
+  BACKEND_BASE_URL: "https://medicback.onrender.com",
+  ENDPOINTS: {
+    DASHBOARD: "/api/dashboard.php",
+    PATIENTS: "/api/patients.php",
+    APPOINTMENTS: "/api/appointments.php",
+    MESSAGE_CENTER: "/api/message_center.php"
+  },
 };
 
